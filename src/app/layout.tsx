295805ae2f5a24bc98420c2a3b98d9c3eaf84f18
Joel Mclean <joel.mclean@nutracheck.co.uk>
2025-05-01 19:01:52 +0100

    Remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Metamorphous } from "next/font/google";
+import { Metamorphous } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const metamorphous = Metamorphous({
   weight: "400",
