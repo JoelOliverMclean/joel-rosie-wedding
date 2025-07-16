e5b410e2678565ac4106081dc3e8ff33e76655c2
Joel Oliver Mclean <joelolivermclean@gmail.com>
2025-07-16 10:58:45 +0100

    Revert to d7f50e1

--- src/utils/formUtils.ts
+++
@@ -1,6 +0,0 @@
-import { FormEvent } from "react";
-
-export const getFormJson = (event: FormEvent<HTMLFormElement>) => {
-  const formData = new FormData(event.currentTarget);
-  return Object.fromEntries(formData.entries());
-};
