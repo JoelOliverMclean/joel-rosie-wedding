e6e688b4ddb527a7120f29ac407825cf47c65524
Joel Oliver Mclean <joelolivermclean@gmail.com>
2026-05-04 12:21:17 +0100

    Loading spinner when fetching RSVP

--- src/app/rsvp/InviteLookupComponent.tsx
+++ src/app/rsvp/InviteLookupComponent.tsx
@@ -3,16 +3,20 @@
 import { InviteSummary } from "@/app/rsvp/types";
 import { useCallback, useEffect, useState } from "react";
 import { apiGet } from "@/utils/apiUtils";
+import { Loader2 } from "lucide-react";
 
 export default function InviteLookup(props: {
   rsvpCode?: string;
   onInviteSelected: (invite: InviteSummary) => void;
 }) {
   const [code, setCode] = useState(() => props.rsvpCode ?? "");
+  const [checking, setChecking] = useState(false);
 
   const handleSubmit = useCallback(() => {
+    setChecking(true);
     const value = code.trim();
     apiGet(`/api/family/by-code?code=${value}`).then(({ response, data }) => {
+      setChecking(false);
       if (response.ok) {
         props.onInviteSelected(data)
       } else {
@@ -32,20 +36,24 @@ export default function InviteLookup(props: {
   return (
     <div className="flex self-stretch items-center justify-center">
       <div className="card flex flex-col gap-5">
-        <div>Enter the RSVP code on your invite</div>
+        { checking ? (<>
+          <Loader2 className={"animate-spin"} />
+        </>) : (<>
+          <div>Enter the RSVP code on your invite</div>
 
-        <input
-          className="text-center"
-          type="text"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-          autoComplete="off"
-          inputMode="text"
-        />
+          <input
+            className="text-center"
+            type="text"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            autoComplete="off"
+            inputMode="text"
+          />
 
-        <button className="btn btn--primary" onClick={handleSubmit}>
-          Find Invite
-        </button>
+          <button className="btn btn--primary" onClick={handleSubmit}>
+            Find Invite
+          </button>
+        </>)}
       </div>
     </div>
   );
