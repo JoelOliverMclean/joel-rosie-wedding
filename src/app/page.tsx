b3e51544b0d18d20a86cd96a1bd4c047b843af92
Joel Oliver Mclean <joelolivermclean@gmail.com>
2026-05-04 17:12:30 +0100

    Fix homepage image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,92 +11,32 @@ import photo from "@/images/homepagephoto.jpeg"
 export default function HomePage() {
   return (
     <div className="stack">
-      {/* Hero */}
-      <section className="hero">
-        {/*<div className="pill">*/}
-        {/*  Autumn • Lanternlight • Cosy Evening*/}
-        {/*</div>*/}
+      <section className="hero flex flex-col gap-10 py-5">
+        <div className={"hero__grid"}>
+          <div className="hero__left text-center">
+            <h1 className="h1">
+              You are invited
+              <span className="h1__sub">to celebrate with us in</span>
+            </h1>
+            <div className={"pt-5"}>
+              <WeddingCountdown targetIso={weddingDate} />
+            </div>
+            <div className={"muted small p-2"}>31st October 2026</div>
 
-        {/*<div className="hero__grid">*/}
-        <div className="text-center">
-          <h1 className="h1">
-            You are invited
-            <span className="h1__sub">to celebrate with us in</span>
-          </h1>
-          <div className={"pt-5"}>
-            <WeddingCountdown targetIso={weddingDate} />
+            <div className="quote">
+              <span className="quote__line" />
+              <p className="quote__text">
+                {'"There and back again... together"'}
+              </p>
+              <span className="quote__line" />
+            </div>
           </div>
 
-          {/*<p className="lead">*/}
-          {/*  Join us beneath turning leaves for an evening of warmth,*/}
-          {/*  candlelight, and quiet storybook magic.*/}
-          {/*</p>*/}
-
-          {/*<div className="actions">*/}
-          {/*  <a className="btn btn--ghost" href="#info">*/}
-          {/*    Day details*/}
-          {/*  </a>*/}
-          {/*  <a className="btn btn--ghost" href="#gallery">*/}
-          {/*    Photos*/}
-          {/*  </a>*/}
-          {/*</div>*/}
-
-          <div className="quote">
-            <span className="quote__line" />
-            <p className="quote__text">
-              {/*“All we have to decide is what to do with the time that is given us.”*/}
-              {'"There and back again... together"'}
-            </p>
-            <span className="quote__line" />
-          </div>
-
-          <div className={"overflow-clip rounded-full my-10"}>
-            <Image src={photo} alt={""} />
-          </div>
-          {/*</div>*/}
-
-          {/* Storybook card */}
-          {/*<aside className="card card--hero">*/}
-          {/*  <div className="eyebrow">The Gathering</div>*/}
-          {/*  <h2 className="h2">Saturday 31st October 2026</h2>*/}
-          {/*  <p className="small muted">*/}
-          {/*    Ceremony at <strong>1:00pm</strong> <br />*/}
-          {/*    Food &amp; drinks to follow <br />*/}
-          {/*    Party starts at <strong>7:00pm</strong>*/}
-          {/*  </p>*/}
-
-          {/*  <div className="card__grid">*/}
-          {/*    <div className="panel">*/}
-          {/*      <div className="panel__title">Location</div>*/}
-          {/*      <div className="small muted flex items-center gap-5">*/}
-          {/*        <div className={"flex-1"}>*/}
-          {/*          <strong>Rufford Mill Wedding Venue, Nottinghamshire</strong>*/}
-          {/*        </div>*/}
-          {/*        <a*/}
-          {/*          href="https://maps.app.goo.gl/KwUC8Tqdo2nVDHhR8"*/}
-          {/*          target={"_blank"}*/}
-          {/*        >*/}
-          {/*          <div className={"btn btn--primary"}>*/}
-          {/*            <Map />*/}
-          {/*          </div>*/}
-          {/*        </a>*/}
-          {/*      </div>*/}
-          {/*    </div>*/}
-          {/*    <div className="panel">*/}
-          {/*      <div className="panel__title">Dress</div>*/}
-          {/*      <div className="small muted">*/}
-          {/*        Day: <strong>Formal</strong> <br />*/}
-          {/*        Evening: <strong>Optional Fancy Dress</strong>*/}
-          {/*      </div>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-
-          {/*  <a className="btn btn--primary card__cta" href="/rsvp">*/}
-          {/*    RSVP here*/}
-          {/*  </a>*/}
-
-          {/*  <span className="seal" aria-hidden="true" />*/}
-          {/*</aside>*/}
+          <aside>
+            <div className={"card self-center overflow-clip p-0! mx-25 lg:mx-auto"}>
+              <Image src={photo} alt={"us"} />
+            </div>
+          </aside>
         </div>
       </section>
 
