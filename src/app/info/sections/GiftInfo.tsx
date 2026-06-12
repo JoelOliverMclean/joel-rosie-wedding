0030c7f69d4fce7ade5139f17865f01710931bbf
Joel Oliver Mclean <joelolivermclean@gmail.com>
2026-06-12 23:21:04 +0100

    Remove weird first gift paragraph

--- src/app/info/sections/GiftInfo.tsx
+++ src/app/info/sections/GiftInfo.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Image from "next/image";
 
-import moneyplease from "@/images/moneyplease.gif"
+import moneyplease from "@/images/moneyplease.gif";
 
 export default function GiftInfo() {
   return (
     <div className={"flex flex-col items-start gap-5"}>
+      {/*<div>*/}
+      {/*  {*/}
+      {/*    "We weren't quite sure how to word this without it sounding awkward, so let's just pretend this isn't a strange thing to put on a website."*/}
+      {/*  }*/}
+      {/*</div>*/}
       <div>
         {
-          "We weren't quite sure how to word this without it sounding awkward, so let's just pretend this isn't a strange thing to put on a website."
+          "We have everything we need, apart from money - but please don't feel any obligation."
         }
       </div>
-      <div>{"We have everything we need, apart from money - but please don't feel any obligation."}</div>
       <div>
         {
           "However if you would like to give a gift, a contribution towards our future/honeymoon would be hugely appreciated."
