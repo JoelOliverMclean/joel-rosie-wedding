b2cd0288e46e828b87cbda21433cf2848c9e2572
Joel Oliver Mclean <joelolivermclean@gmail.com>
2026-06-13 11:20:47 +0100

    Editable RSVP Codes in admin

--- src/app/(admin)/admin/family/[id]/page.tsx
+++ src/app/(admin)/admin/family/[id]/page.tsx
@@ -1,7 +1,11 @@
-import { getFamily, updateGuest } from "@/app/(admin)/admin/family/[id]/actions";
+import {
+  getFamily,
+  updateGuest,
+  updateRSVPCode,
+} from "@/app/(admin)/admin/family/[id]/actions";
 import { Guest } from "@/lib/prisma-types";
 import EditGuestForm from "./guest-form";
-import { revalidatePath } from "next/cache";
+import EditRSVPForm from "@/app/(admin)/admin/family/[id]/edit-rsvp-form";
 
 export default async function FamilyPage({
   params,
@@ -15,17 +19,28 @@ export default async function FamilyPage({
 
   const onUpdateGuest = async (guest: Guest) => {
     "use server";
-    await updateGuest(guest)
-    revalidatePath(`admin/family/${id}`)
+    await updateGuest(guest);
+  };
+  const onUpdateRSVPCode = async (rsvpCode: string) => {
+    "use server";
+    if (family) await updateRSVPCode(family.id, rsvpCode);
   };
 
   return (
     <div className={"section flex flex-col gap-5"}>
       <p className={"muted"}>Family</p>
       <h1>{family?.familyName}</h1>
-      {guests.map((guest, index) =>
-        <EditGuestForm key={index} guest={guest} onUpdateGuest={onUpdateGuest} />
-      )}
+      {guests.map((guest, index) => (
+        <EditGuestForm
+          key={index}
+          guest={guest}
+          onUpdateGuest={onUpdateGuest}
+        />
+      ))}
+      <EditRSVPForm
+        rsvpCode={family?.rsvpCode}
+        onUpdateRSVPCode={onUpdateRSVPCode}
+      />
     </div>
   );
 }
--- src/app/(admin)/admin/family/page.tsx
+++ src/app/(admin)/admin/family/page.tsx
@@ -1,5 +1,6 @@
 ﻿import { prisma } from "@/lib/prisma";
 import Link from "next/link";
+import { regenerateRSVPCodes } from "@/app/(admin)/admin/family/actions";
 
 export const dynamic = "force-dynamic";
 
@@ -14,30 +15,40 @@ export default async function FamilyAdminPage() {
     },
   });
 
+  const regenerateCodes = async () => {
+    "use server";
+    await regenerateRSVPCodes();
+  };
+
   return (
     <div className={"section flex flex-col gap-5"}>
-      <h1 className={"h1"}>Families</h1>
+      <div className={"flex items-center justify-between"}>
+        <h1 className={"h1"}>Families</h1>
+        <button onClick={regenerateCodes} className={"btn btn--ghost"}>
+          Regenerate Codes
+        </button>
+      </div>
       {!families ? (
-        <div>Waiting...</div>
+        <div>Loading...</div>
       ) : (
-        <div
-          className={
-            "grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3"
-          }
-        >
-          {families.map((family) => (
-            <div key={family.id} className={"pill flex flex-col gap-2"}>
-              <h2 className={"h2 text-center"}>{family.familyName}</h2>
-              <div className={"flex flex-wrap justify-center gap-2"}>
-                <div>-</div>
-                {family.guests.map((guest) => (
-                  <div key={guest.id} className={""}>
-                    {guest.firstName} -
-                  </div>
-                ))}
+        <>
+          <div className={"flex flex-col"}>
+            {families.map((family, i) => (
+              <div
+                key={i}
+                className={`grid grid-cols-4 gap-5 p-2 ${i < families.length - 1 ? "border-b-1 border-white" : ""}`}
+              >
+                <h2 className={"h2"}>
+                  {family.id} - {family.familyName}
+                </h2>
+                <div className={"col-span-2 flex flex-wrap gap-2"}>
+                  <p>{family.guests.map((g) => g.firstName).join(", ")}</p>
+                </div>
+                <p>{family.rsvpCode}</p>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
           <Link
             href={"/admin/family/new"}
             key={-1}
@@ -45,7 +56,7 @@ export default async function FamilyAdminPage() {
           >
             Add new
           </Link>
-        </div>
+        </>
       )}
     </div>
   );
